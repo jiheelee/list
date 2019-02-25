73141ca1a7c376af43cde5f8eb56ec61232c5cf6
jiheelee <jiheelee.ljh@gmail.com>
2019-02-25 17:57:03 +0900

    알고리즘

--- 더하기/plus.py
+++ 더하기/plus.py
@@ -4,7 +4,7 @@ for tc in range(1,T+1):
     num_list = list(map(int,input().split()))
     sum_list=[]
     for length in range(1,N+1):
-        for i in range(N-length,0,-1):
+        for i in range(N-length,-1,-1):
             part = 0
             for j in range(length):
                 part += num_list[j]
